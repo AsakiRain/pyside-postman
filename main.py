fbd4b38d78da1c62a534f6b79b319aa3018837a9
AsakiRain <1287059631@qq.com>
2022-09-08 11:14:09 +0800

    feat: popup dialog editor

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 from ui.ui_postman import Ui_MainWindow
+from ui.ui_dialogEditor import Ui_dialogEditor
 import json
 
 
@@ -26,15 +27,71 @@ class PostMan(QMainWindow):
             self.tModel.appendRow(
                 [item0, QStandardItem(key), QStandardItem(value)])
         self.ui.table.setModel(self.tModel)
+        self.ui.table.doubleClicked.connect(self.handle_edit)
         self.ui.table.setItemDelegateForColumn(
             0, ReadOnlyDelegate(self.ui.table))
         self.ui.table.setItemDelegateForColumn(
             1, LineEditDelegate(self.ui.table))
+        self.ui.table.setItemDelegateForColumn(
+            2, ReadOnlyDelegate(self.ui.table))
 
     def read_default_headers(self):
         file = open('default_headers.json', 'r')
         data = json.loads(file.read())
         return data.items()
+
+    def handle_edit(self, index: QModelIndex):
+        if(index.column() == 2):
+            self.dEditor = DialogEditor()
+            self.dEditor.show()
+            key_index = QModelIndex(index.model().index(index.row(), 1))
+            value_index = QModelIndex(index.model().index(index.row(), 2))
+            key = index.model().data(key_index)
+            value = index.model().data(value_index)
+
+            self.dEditor.key_index = key_index
+            self.dEditor.value_index = value_index
+            self.dEditor.setValue(key, value)
+            self.dEditor.accept_signal.connect(self.handle_accept)
+            self.dEditor.exec_()
+
+    def handle_accept(self, key, value):
+        if(self.dEditor.newItem):
+            item0 = QStandardItem()
+            item0.setCheckable(True)
+            self.tModel.appendRow(
+                [item0, QStandardItem(key), QStandardItem(value)])
+        else:
+            self.tModel.setData(self.dEditor.key_index, key)
+            self.tModel.setData(self.dEditor.value_index, value)
+
+
+class DialogEditor(QDialog):
+    accept_signal = Signal(str, str)
+
+    def __init__(self):
+        super().__init__()
+        self.newItem = False
+        self.key_index: QModelIndex = None
+        self.value_index: QModelIndex = None
+        self.ui = Ui_dialogEditor()
+        self.ui.setupUi(self)
+
+    def accept(self):
+        key = self.ui.keyEdit.text()
+        value = self.ui.valueEdit.toPlainText()
+        if(not key or not value):
+            QMessageBox.warning(self, '警告', '字段不能为空')
+        else:
+            self.accept_signal.emit(key, value)
+            return super().accept()
+
+    def reject(self):
+        return super().reject()
+
+    def setValue(self, key, value):
+        self.ui.keyEdit.setText(key)
+        self.ui.valueEdit.setText(value)
 
 
 class ReadOnlyDelegate(QItemDelegate):
